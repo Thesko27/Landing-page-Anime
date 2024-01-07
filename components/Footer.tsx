171acd6fe6667b9cf19a0ddb9822a364327b79e0
Thesko27 <franthesko1984@gmail.com>
2024-01-07 08:48:14 -0300

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
             <p className="text-base font-bold text-white">
-                @2024 /landingPageAnime
+                @2024/ LandingPageAnime
             </p>
             <Image
                 src="./logo.svg"
